005806d7893562961b1786fec25e00207988cf34
Brady Walters <49881821+bray18@users.noreply.github.com>
2024-11-02 13:32:15 -0500

    Enhance TransWindow method to accept parameters for GIF path, file name, and frame control; update animation loading logic

--- mainController.py
+++ mainController.py
@@ -8,7 +8,8 @@ class mainController:
         self.sheepview = sheepview
 
     def openSheep(self):
-        self.sheepview.TransWindow()
+        self.sheepview.TransWindow('SheepAnimations\\', 'SheepEat.gif', 0, 3)
+        #self.sheepview.TransWindow('SheepAnimations\\', 'growth.gif', 3, 3)
 
     def openMainMenu(self):
         self.mainview.MainMenu()
--- sheepViews.py
+++ sheepViews.py
@@ -7,22 +7,33 @@ bg = '#ffa3bb'
 
 class sheepViews():
 
-    def TransWindow(self):
+    def TransWindow(self, gif_path, gif_file_name, current_frame, total_frames):
         window = tkinter.Tk()
-        path = 'SheepAnimations\\'
         window.config(highlightbackground=bg) 
-        label = tkinter.Label(window,bd=0,bg=bg)
+        label = tkinter.Label(window, bd=0, bg=bg)
         window.overrideredirect(True)
-        window.wm_attributes('-transparentcolor',bg) # make the background transparent based on hex code of the background color
-        animation = [tkinter.PhotoImage(file=path+'Growth.gif',format = 'gif -index %i' %(i)) for i in range(3)]#idle gif
-        frame = animation[0]
-        width = window.winfo_screenwidth()
-        height = window.winfo_screenheight()
-        #TODO: calculate this bigFrame size based on screen size becuase whiteboxing is bad. thanks github
-        window.geometry(str(int(width/20))+'x'+str(int(width/20))) #GROSS.
-        bigFrame = frame.zoom(int((width/20)/32),int((width/20)/32))
-        label.configure(image=bigFrame)
+        window.wm_attributes('-transparentcolor', bg)  # make the background transparent based on hex code of the background color
+        
+        # Load the GIF frames
+        animation_frames = [tkinter.PhotoImage(file=gif_path + gif_file_name, format='gif -index %i' % i) for i in range(total_frames)]
+        current_gif_frame = animation_frames[current_frame]
+        
+        # Get screen dimensions
+        screen_width = window.winfo_screenwidth()
+        screen_height = window.winfo_screenheight()
+        
+        # Calculate window size based on screen size
+        window_size = int(screen_width / 20)
+        window.geometry(f'{window_size}x{window_size}')
+        
+        # Resize the GIF frame
+        resized_frame = current_gif_frame.zoom(window_size // 32, window_size // 32)
+        label.configure(image=resized_frame)
         label.pack()
+        
+        # Keep the window on top
+        window.attributes('-topmost', True)
+        
         window.mainloop()
         #make it stay on top
 
