a6b006c9a72bf656909c2374376993a73efe7186
Brady Walters <49881821+bray18@users.noreply.github.com>
2024-11-02 03:13:34 -0500

    kinda messy, but what we need in the main views is there now. Not hooked up to anything, but hey, I was mostly working on the view intentionally so I think I did my job and now I get to go to sleep! Isn't that wonderful. Good night everyone, we'll see you next week!

--- mainViews.py
+++ mainViews.py
@@ -19,20 +19,35 @@ def MainMenu():
     query = Label(root, text='Name your sheep: ').grid(row=1)
     sheepName = Entry(root).grid(row=1,column=1) # query and sheepName are both row 1, but the name is next
     # to query so it has column 1
-    tQuery = Label(root, text='What is your task?').grid(row=2, column=0)
-    task = Entry(root, textvariable=taskItem).grid(row=2,column=1)
+    task = Label(root, text='Enter your task: ').grid(row=2)
+    taskEntry = Entry(root, textvariable=taskItem).grid(row=2,column=1)
     
-    tlBox = Listbox(root).grid(row=3)
-    for t in taskList:
-        tlBox.insert(END,t)
+    # Time inputs. 2 small inline number inputs for minutes and hours in each with preset times of 25 and 5
+    Label(root, text='Work time: ').grid(row=3)
+    workTimeHours = Entry(root, width=2)
+    workTimeHours.insert(0, '0')  # preset number
+    workTimeHours.grid(row=3, column=1)
+    Label(root, text='hour(s)').grid(row=3, column=2)
+    workTimeMinutes = Entry(root, width=2)
+    workTimeMinutes.insert(0, '25')  # preset number
+    workTimeMinutes.grid(row=3, column=3)
+    Label(root, text='minute(s)').grid(row=3, column=4)
+    
+    Label(root, text='Break time: ').grid(row=4)
+    breakTimeHours = Entry(root, width=2)
+    breakTimeHours.insert(0, '0')  # preset number
+    breakTimeHours.grid(row=4, column=1)
+    Label(root, text='hour(s)').grid(row=4, column=2)
+    breakTimeMinutes = Entry(root, width=2)
+    breakTimeMinutes.insert(0, '5')  # preset number
+    breakTimeMinutes.grid(row=4, column=3)
+    Label(root, text='minute(s)').grid(row=4, column=4)
+    
+    
 
-    newTask = Button(root, text='ADD', command=FillTaskList(taskItem,taskList)).grid(row=6,column=0)
-
-    quit = Button(root, text='EXIT', command = root.quit).grid(row=6,column=1)
+    quit = Button(root, text='EXIT', command = root.quit).grid(row=6,column=0)
+    go = Button(root, text='GO').grid(row=6,column=1) # starts the little sheep animation guy thing I am very tired
     root.mainloop()
-
-def FillTaskList(task, taskList):
-    taskList.append(str(task) + '\n')
 
 
 MainMenu()
