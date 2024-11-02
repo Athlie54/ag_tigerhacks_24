d9fdfeb1bcfe8d013c118edb9214283d153fd9d8
Brady Walters <49881821+bray18@users.noreply.github.com>
2024-11-02 16:30:17 -0500

    every step you take

--- sheepViews.py
+++ sheepViews.py
@@ -1,7 +1,5 @@
-# sheep window! sheep window!
 import tkinter
 from tkinter import *
-#import win32api
 
 bg = '#ffa3bb'
 
@@ -9,16 +7,11 @@ class sheepViews():
 
     def TransWindow(self, gif_path, gif_file_name, current_frame, total_frames):
         window = tkinter.Tk()
-        canvas = tkinter.Canvas(window, width=200, height=100, bg=bg)
+        canvas = tkinter.Canvas(window, width=200, height=100, bg=bg, highlightthickness=0)
         canvas.pack(fill="both", expand=True)
         window.wm_attributes('-transparentcolor', bg)  # make the background transparent based on hex code of the background color
-        window.config(highlightbackground=bg) 
-        label = tkinter.Label(canvas, text="label1", bd=0, bg=bg)
-        canvas.create_window(10,10, window=label)
-        label2 = tkinter.Label(canvas, text="label2", bd=0, bg=bg)
-        canvas.create_window(50,50, window=label2)
         window.overrideredirect(True)
-                
+        
         # Load the GIF frames
         animation_frames = [tkinter.PhotoImage(file=gif_path + gif_file_name, format='gif -index %i' % i) for i in range(total_frames)]
         current_gif_frame = animation_frames[current_frame]
@@ -33,22 +26,16 @@ class sheepViews():
         
         # Resize the GIF frame
         resized_frame = current_gif_frame.zoom(window_size // 32, window_size // 32)
-        label.configure(image=resized_frame)
-        label.image = resized_frame
-        
+        canvas.create_image(10, 10, anchor=NW, image=resized_frame)
         
         resized_frame2 = tkinter.PhotoImage(file="SheepAnimations\\Shammy.png").zoom(window_size // 32, window_size // 32)
-        label2.configure(image=resized_frame2)
-        label2.image = resized_frame2
-        label.pack()
-        label2.pack()
-        
+        canvas.create_image(50, 50, anchor=NW, image=resized_frame2)
         
         # Keep the window on top
         window.attributes('-topmost', True)
         
         window.mainloop()
-        #make it stay on top
 
-    # def bodyWindow(self):
-    # SheepWindow()
+# Example usage
+# sheep_view = sheepViews()
+# sheep_view.TransWindow('path_to_gif/', 'gif_file_name.gif', 0, 10)
