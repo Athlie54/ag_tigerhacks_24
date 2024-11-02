10f6252133bed9d0311d726e470825dd68c26cbf
Athlie54 <kjself04@gmail.com>
2024-11-01 20:37:17 -0500

    basic window babey!!

--- sheepViews.py
+++ sheepViews.py
@@ -1,1 +1,14 @@
+from tkinter import *
+
+root = Tk()
+
 # main menu/view(s)
+def mainMenu():
+    title = Label(root, text='Welcome to the Flock!').grid(row=0)
+    query = Label(root, text='Name your sheep: ').grid(row=1)
+    sheepName = Entry(root).grid(row=1,column=1)
+    done = Button(root, text='DONE', command = root.quit).grid(row=2)
+    root.mainloop()
+
+
+mainMenu()
