d503eb4086ecf6cc79f42cc0c12bf0dc3c57c703
Athlie54 <kjself04@gmail.com>
2024-11-02 23:25:22 -0500

    changing from datetime to time in taskClass

--- taskClass.py
+++ taskClass.py
@@ -1,27 +1,38 @@
 # task class, gets the task name, time to work, time to rest
 
-import datetime
+import time
 
 class Task:
     def __init__(self, taskName, timeToWork, timeToRest):
         self.taskName = taskName
         self.timeToWork = timeToWork
         self.timeToRest = timeToRest
-        self.timeStarted = datetime.datetime.now()
-        self.timeRemaining = None
-        self.timeEnded = None
+        self.timeStarted = 0
+        self.timeRemaining = 0
+        self.timeEnded = ""
         self.isWorking = True
         #print(self.taskName,self.timeToWork,self.timeToRest)
 
-    def start(self):
-        self.timeStarted = datetime.datetime.now()
-        self.timeRemaining = self.timeToWork
-        self.isWorking = True
+    # def start(self):
+    #     self.timeStarted = datetime.datetime.now()
+    #     self.timeRemaining = self.timeToWork
+    #     self.isWorking = True
 
-    def stop(self):
-        self.timeEnded = datetime.datetime.now()
-        self.timeRemaining = None
-        self.isWorking = False
+    # def stop(self):
+    #     self.timeEnded = datetime.datetime.now()
+    #     self.timeRemaining = None
+    #     self.isWorking = False
+
+    # if param given is 0, then user is working; if 1, user is resting
+    def setState(self, state):
+            if(state==0):
+                self.timeStarted = time.time()
+                self.timeRemaining = self.timeToWork
+                self.isWorking = True
+            elif(state==1):
+                self.timeEnded = time.time()
+                self.timeRemaining = 0
+
 
     def rest(self):
         self.isWorking = False
@@ -51,3 +62,4 @@ class Task:
 
     def getIsWorking(self):
         return self.isWorking
+
