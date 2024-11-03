154c94148799ead648c9cb5b6d16645644af9e2a
KCSBach <112579594+KCSBach@users.noreply.github.com>
2024-11-03 07:14:58 -0600

    make stuff happen in init in sheepview

--- animationController.py
+++ animationController.py
@@ -8,64 +8,70 @@ class animationController:
     # INIT, BLASTER AND SETTERS
     def __init__(self, sheepview):
         self.sheepview = sheepview
-        self.layer1 = ["TransSheep\\", "SheepEatBodyTrans.gif", 0]
-        self.layer2 = ["TransSheep\\", "GrowthTrans.gif", 0]
-        self.layer3 = ["TransSheep\\", "SheepEatHeadTrans.gif", 0]
+        #self.layer1 = ["TransSheep\\", "SheepEatBodyTrans.gif", 0]
+        #self.layer2 = ["TransSheep\\", "GrowthTrans.gif", 0]
+        #self.layer3 = ["TransSheep\\", "SheepEatHeadTrans.gif", 0]
+        self.layer1Frame = 0
+        self.layer2Frame = 0
+        self.layer3Frame = 0
         
     def blastLayersToSheep(self):
-        self.sheepview.TransWindow(self.layer1[0], self.layer1[1], self.layer1[2], self.layer2[0], self.layer2[1], self.layer2[2], self.layer3[0], self.layer3[1], self.layer3[2])
+        self.sheepview.TransWindow(self.layer1Frame,self.layer2Frame,self.layer3Frame)#self.layer1[0], self.layer1[1], self.layer1[2], self.layer2[0], self.layer2[1], self.layer2[2], self.layer3[0], self.layer3[1], self.layer3[2]
         
-    def setLayer1(self, gif_path, gif_file_name, current_frame):
-        self.layer1[0] = gif_path
-        self.layer1[1] = gif_file_name
-        self.layer1[2] = current_frame
+    def setLayer1(self, index):#gif_path, gif_file_name, current_frame
+        self.layer1Frame = index
+        #self.layer1[0] = gif_path
+        #self.layer1[1] = gif_file_name
+        #self.layer1[2] = current_frame
         
-    def setLayer2(self, gif_path, gif_file_name, current_frame):
-        self.layer2[0] = gif_path
-        self.layer2[1] = gif_file_name
-        self.layer2[2] = current_frame
+    def setLayer2(self, index):
+        self.layer2Frame = index
+        # self.layer2[0] = gif_path
+        # self.layer2[1] = gif_file_name
+        # self.layer2[2] = current_frame
         
-    def setLayer3(self, gif_path, gif_file_name, current_frame):
-        self.layer3[0] = gif_path
-        self.layer3[1] = gif_file_name
-        self.layer3[2] = current_frame
+    def setLayer3(self, index):
+        self.layer3Frame = index
+        # self.layer3[0] = gif_path
+        # self.layer3[1] = gif_file_name
+        # self.layer3[2] = current_frame
         
     #STATIC POSES AND SUCH
     
     def SheepGrowth(self, level):
-        self.setLayer2("TransSheep\\", "GrowthTrans.gif", level)
+        self.setLayer2(level)#"TransSheep\\", "GrowthTrans.gif"
     
     def SheepIdle(self):
-        self.setLayer1("TransSheep\\", "SheepEatBodyTrans.gif", 0)
-        self.setLayer3("TransSheep\\", "SheepEatHeadTrans.gif", 0)
+        self.setLayer1(0)#"TransSheep\\", "SheepEatBodyTrans.gif", 0
+        self.setLayer3(0)#"TransSheep\\", "SheepEatHeadTrans.gif", 0
     
     #ANIMATIONS (TIMED)
     
     def SheepEat(self):
-        self.setLayer1("TransSheep\\", "SheepEatBodyTrans.gif", 0)
-        self.setLayer3("TransSheep\\", "SheepEatHeadTrans.gif", 0)
+        self.setLayer1(0)#"TransSheep\\", "SheepEatBodyTrans.gif"
+        self.setLayer3(0)#"TransSheep\\", "SheepEatHeadTrans.gif", 
         for i in range(15):
-            self.setLayer3("TransSheep\\", "SheepEatHeadTrans.gif", i)
+            self.setLayer3(i)#"TransSheep\\", "SheepEatHeadTrans.gif", 
             self.blastLayersToSheep()
             time.sleep(self.waitTime)
             
         self.SheepIdle()
         
     def SheepShocked(self):
-        self.setLayer1("TransSheep\\", "SheepEatTrans.gif", 0)
+        self.setLayer1(0+1)#"TransSheep\\", "SheepEatTrans.gif", 
         self.blastLayersToSheep()
         for i in range(10):
-            self.setLayer3("TransSheep\\", "LightningTrans.gif", i)
+            self.setLayer3(i+15)#"TransSheep\\", "LightningTrans.gif", 
             self.blastLayersToSheep()
             time.wait(self.waitTime)
         for i in range(9):
-            self.setLayer1("TransSheep\\", "ShockedSheep.gif", i)
-            self.setLayer3("TransSheep\\", "LightningTrans.gif", 10+i)
+            self.setLayer1(i+2)#"TransSheep\\", "ShockedSheep.gif", 
+            self.setLayer3(10+i+15)#"TransSheep\\", "LightningTrans.gif", 
             self.blastLayersToSheep()
             time.wait(self.waitTime)
         for i in range(15):
-            self.setLayer1("TransSheep\\", "ShockedSheep.gif", 9+i)
-            self.setLayer3("TransSheep\\", "LightningTrans.gif", 16-i)
+            self.setLayer1(9+i+2)#"TransSheep\\", "ShockedSheep.gif", 
+            self.setLayer3(16-i+15)#"TransSheep\\", "LightningTrans.gif", 
             self.blastLayersToSheep()
             time.wait(self.waitTime)
         self.SheepIdle()
--- sheepViews.py
+++ sheepViews.py
@@ -8,23 +8,32 @@ bg = '#ffa3ba'
 class sheepViews():
     def __init__(self):
         self.controller = None
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(self.window, width=200, height=100, bg=bg, highlightthickness=0)
+        self.canvas.pack(fill="both", expand=True)
+        self.window.wm_attributes('-transparentcolor', bg)
+        self.window.overrideredirect(True)
+        
+        self.layer1_1 = (self.window.PhotoImage(file="TransSheep\\SheepEatBodyTrans.gif",format = 'gif -index %i' %(i)) for i in range(1))
+        self.layer1_2 = (self.window.PhotoImage(file="TransSheep\\SheepEatTrans.gif",format = 'gif -index %i' %(i)) for i in range(1))
+        self.layer1_3 = (self.window.PhotoImage(file="TransSheep\\ShockedSheep.gif",format = 'gif -index %i' %(i)) for i in range(24))
+        self.layer1 = [self.layer1_1]+[self.layer1_2]+[self.layer1_3]
+        
+        self.layer2 = [self.window.PhotoImage(file="TransSheep\\GrowthTrans.gif",format = 'gif -index %i' %(i)) for i in range(3)]
+        
+        self.layer3 = [self.window.PhotoImage(file="TransSheep\\SheepEatHeadTrans.gif",format = 'gif -index %i' %(i)) for i in range(15)]+[#"TransSheep\\", "SheepEatHeadTrans.gif", 0
+            self.window.PhotoImage(file="TransSheep\\LightningTrans.gif",format = 'gif -index %i' %(i)) for i in range(18)]
 
     def setController(self,controller):
         self.controller = controller
         
-    def TransWindow(self, gif_path, gif_file_name, current_frame, gif_path2, gif_file_name2, current_frame2, gif_path3, gif_file_name3, current_frame3):
-        window = tkinter.Tk()
-        canvas = tkinter.Canvas(window, width=200, height=100, bg=bg, highlightthickness=0)
-        canvas.pack(fill="both", expand=True)
-        window.wm_attributes('-transparentcolor', bg)
-        window.overrideredirect(True)
-        
+    def TransWindow(self, indexLayer1,indexLayer2,indexLayer3):#gif_path, gif_file_name, current_frame, gif_path2, gif_file_name2, current_frame2, gif_path3, gif_file_name3, current_frame3
         # Load frames for the first GIF
         animation_frames1 = []
         frame_index = 0
         while True:
             try:
-                frame = tkinter.PhotoImage(file=gif_path + gif_file_name, format='gif -index %i' % frame_index)
+                frame = self.layer1[indexLayer1] #tkinter.PhotoImage(file=gif_path + gif_file_name, format='gif -index %i' % frame_index)
                 animation_frames1.append(frame.subsample(1, 1))
                 frame_index += 1
             except tkinter.TclError:
@@ -35,7 +44,7 @@ class sheepViews():
         frame_index = 0
         while True:
             try:
-                frame = tkinter.PhotoImage(file=gif_path2 + gif_file_name2, format='gif -index %i' % frame_index)
+                frame = self.layer2[indexLayer2] #tkinter.PhotoImage(file=gif_path2 + gif_file_name2, format='gif -index %i' % frame_index)
                 animation_frames2.append(frame.subsample(1, 1))
                 frame_index += 1
             except tkinter.TclError:
@@ -46,46 +55,46 @@ class sheepViews():
         frame_index = 0
         while True:
             try:
-                frame = tkinter.PhotoImage(file=gif_path3 + gif_file_name3, format='gif -index %i' % frame_index)
+                frame = self.layer3[indexLayer3] #tkinter.PhotoImage(file=gif_path3 + gif_file_name3, format='gif -index %i' % frame_index)
                 animation_frames3.append(frame.subsample(1, 1))
                 frame_index += 1
             except tkinter.TclError:
                 break
 
         # Set the current frame for each GIF
-        current_gif_frame1 = animation_frames1[current_frame]
-        current_gif_frame2 = animation_frames2[current_frame2]
-        current_gif_frame3 = animation_frames3[current_frame3]
+        # current_gif_frame1 = animation_frames1[current_frame]
+        # current_gif_frame2 = animation_frames2[current_frame2]
+        # current_gif_frame3 = animation_frames3[current_frame3]
         
         # Get screen dimensions
-        screen_width = window.winfo_screenwidth()
-        screen_height = window.winfo_screenheight()
+        screen_width = self.window.winfo_screenwidth()
+        screen_height = self.window.winfo_screenheight()
         
         # Calculate window size
         window_size = int(screen_width / 16)
-        window.geometry(f'{window_size}x{window_size}')
+        self.window.geometry(f'{window_size}x{window_size}')
         
         # Resize and display each GIF layer
-        resized_frame1 = current_gif_frame1.zoom(window_size // 32, window_size // 32)
-        canvas.create_image(10, 10, anchor=NW, image=resized_frame1)
+        resized_frame1 = frame.zoom(window_size // 32, window_size // 32)
+        self.canvas.create_image(10, 10, anchor=NW, image=resized_frame1)
 
-        resized_frame2 = current_gif_frame2.zoom(window_size // 32, window_size // 32)
-        canvas.create_image(10, 10, anchor=NW, image=resized_frame2)
+        resized_frame2 = frame.zoom(window_size // 32, window_size // 32)
+        self.canvas.create_image(10, 10, anchor=NW, image=resized_frame2)
 
-        resized_frame3 = current_gif_frame3.zoom(window_size // 32, window_size // 32)
-        canvas.create_image(10, 10, anchor=NW, image=resized_frame3)
+        resized_frame3 = frame.zoom(window_size // 32, window_size // 32)
+        self.canvas.create_image(10, 10, anchor=NW, image=resized_frame3)
         
         
         # Position window in bottom right corner
         x_position = screen_width - window_size - 50
         y_position = screen_height - window_size - 30
-        window.geometry(f'+{x_position}+{y_position}')
+        self.window.geometry(f'+{x_position}+{y_position}')
         
         # Keep window on top
-        window.attributes('-topmost', True)
+        self.window.attributes('-topmost', True)
         
         # Right-click menu
-        sheepMenu = Menu(window, tearoff=0, bg='lightgreen')
+        sheepMenu = Menu(self.window, tearoff=0, bg='lightgreen')
         sheepMenu.add_command(label="Return to Main Menu", command=self.controller.openMainMenu)
         sheepMenu.add_separator()
         sheepMenu.add_command(label="Temp-hide (15 secs)")#command=self.controller.tempHideSheep
@@ -100,11 +109,11 @@ class sheepViews():
             finally:
                 sheepMenu.grab_release()
         
-        window.bind("<Button-3>", popup)
+        self.window.bind("<Button-3>", popup)
 
         
-        window.update_idletasks()
-        window.update()
+        self.window.update_idletasks()
+        self.window.update()
         
 
 # Example usage
